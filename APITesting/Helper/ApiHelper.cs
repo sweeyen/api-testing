a02c31271a040a97886c9dbc07a34deaa0eedc98
sweeyen <sweeyen@gmail.com>
2025-04-03 23:45:50 +0800

    upgrade package

--- APITesting/Helper/ApiHelper.cs
+++ APITesting/Helper/ApiHelper.cs
@@ -14,52 +14,55 @@ namespace APITesting.Helper
         public RestClient SetUrl(String endpoint)
         {
             var url = String.Concat(baseUrl, endpoint);
-            var restClient = new RestClient(url);
+            var options = new RestClientOptions(url);
+            var restClient = new RestClient(options);
             return restClient;
         }
 
         public RestRequest PostRequestWithBody(string request)
         {
-            restRequest = new RestRequest(Method.POST);
+            restRequest = new RestRequest();
+            restRequest.Method = Method.Post;
             restRequest.AddHeader("Accept", "application/json");
-            restRequest.AddParameter("application/json", request, ParameterType.RequestBody);
+            restRequest.AddJsonBody(request);
             return restRequest;
         }
 
         public RestRequest UpdateRequestWithBody(string request)
         {
-            restRequest = new RestRequest(Method.PUT);
+            restRequest = new RestRequest();
+            restRequest.Method = Method.Put;
             restRequest.AddHeader("Accept", "application/json");
-            restRequest.AddParameter("application/json", request, ParameterType.RequestBody);
+            restRequest.AddJsonBody(request);
             return restRequest;
         }
 
-
         public RestRequest DeleteRequest()
         {
-            restRequest = new RestRequest(Method.DELETE);
+            restRequest = new RestRequest();
+            restRequest.Method = Method.Delete;
             restRequest.AddHeader("Accept", "application/json");
             return restRequest;
         }
 
         public RestRequest SendRequest()
         {
-            restRequest = new RestRequest(Method.GET);
+            restRequest = new RestRequest();
+            restRequest.Method = Method.Get;
             restRequest.AddHeader("Accept", "application/json");
             return restRequest;
         }
 
-        public IRestResponse GetResponse(RestClient restClient,RestRequest restRequest)
+        public RestResponse GetResponse(RestClient restClient, RestRequest restRequest)
         {
             return restClient.Execute(restRequest);
         }
 
-        public Attributes GetContent<Attributes>(IRestResponse restResponse)
+        public Attributes GetContent<Attributes>(RestResponse restResponse)
         {
             var content = restResponse.Content;
             Attributes attribute = JsonConvert.DeserializeObject<Attributes>(content);
             return attribute;
         }
-
     }
 }
