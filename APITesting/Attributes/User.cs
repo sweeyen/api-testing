a2e64c986d37c6bdc16634712401a4febf44ddfa
sweeyen <sweeyen@gmail.com>
2025-09-06 12:18:49 +0800

    update test cases to add more header

--- APITesting/Attributes/User.cs
+++ APITesting/Attributes/User.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace APITesting.Attributes
 {
-    public partial class User
+    public class User
     {
         [JsonProperty("name")]
         public string Name { get; set; }
@@ -12,41 +12,9 @@ namespace APITesting.Attributes
         public string Job { get; set; }
 
         [JsonProperty("id")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long Id { get; set; }
+        public string Id { get; set; }
 
         [JsonProperty("createdAt")]
         public DateTime CreatedAt { get; set; }
-
-
-        internal class ParseStringConverter : JsonConverter
-        {
-            public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
-
-            public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
-            {
-                if (reader.TokenType == JsonToken.Null) return null;
-                var value = serializer.Deserialize<string>(reader);
-                long l;
-                if (Int64.TryParse(value, out l))
-                {
-                    return l;
-                }
-                throw new Exception("Cannot unmarshal type long");
-            }
-
-            public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-            {
-                if (untypedValue == null)
-                {
-                    serializer.Serialize(writer, null);
-                    return;
-                }
-                var value = (long)untypedValue;
-                serializer.Serialize(writer, value.ToString());
-                return;
-            }
-
-        }
     }
 }
